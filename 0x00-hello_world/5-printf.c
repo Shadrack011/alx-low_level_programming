ecc5f980a0b8ceaa73861d5f8b0e3cbfa10bd483
Shadrack011 <shadrackomondi4@gmail.com>
2022-07-02 20:17:42 -0700

    dobe

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main function
+ * description - prints with proper grammar, but the outcome is a piece of art
+ * Return: 0 always
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
