bd5fd85ffad0c81e7c1b71ee2d0572e2516b11cc
Shadrack011 <shadrackomondi4@gmail.com>
2022-07-04 14:58:34 -0700

    prints all possible different combinations of 3 digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 48; a <= 57; a++)
+	for (a = 48; a < 57; a++)
 	{
 		for (b = a + 1; b <= 57; b++)
 		{
 			putchar(a);
 			putchar(b);
-			if (a == 57 && b == 57)
+			if (a == 56 && b == 57)
 				continue;
 			putchar(',');
 			putchar(' ');
