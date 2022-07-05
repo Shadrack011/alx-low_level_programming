716eaa502af1dc6f0b0ad9c90f2d325a28411275
Shadrack011 <shadrackomondi4@gmail.com>
2022-07-05 16:42:20 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int x;
+	int n;
 
-	if (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+	n = n >= 0 ? n : n * -1;
+	int last = n % 10;
+
+	_putchar('0' + last);
+	return (last);
+
 }
