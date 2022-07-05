b25d9db86b30cfc573392af070fbd2e512882315
Shadrack011 <shadrackomondi4@gmail.com>
2022-07-05 15:21:08 -0700

    prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @numa: first integer to be added
+ * @numb: second integer to be added
+ * Return: results of adding the two integers
+ */
+int add(int numa, int numb)
+{
+	return (numa + numb);
+}
