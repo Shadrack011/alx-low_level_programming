383854c77e1ba35c8ea8a5d0d5f6579a3b71559b
Shadrack011 <shadrackomondi4@gmail.com>
2022-07-11 09:11:38 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ *@str: pointer to the string
+ *
+ */
+
+void _puts(char *str)
+{
+	_putchar(*str);
+	_putchar('\n');
+}
